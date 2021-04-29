c19ab27161cc76ee062a1e1c9d61777e7c478988
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-04-29 06:45:13 +0200

    modify for heroku

--- database.js
+++ database.js
@@ -1,6 +1,14 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-hamsterwars-key.json");
+//const serviceAccount = require("./firebase-hamsterwars-key.json");
+//serviceAccount ändras för HEROKU
+let serviceAccount;
+
+if( process.env.PRIVATE_KEY ){
+	serviceAccount = JSON.parse( process.env.PRIVATE_KEY )
+} else {
+	serviceAccount = require("./firebase-hamsterwars-key.json")
+}
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
