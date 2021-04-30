b2c323a1648e78c0a3dd81485f6ada42b2137b93
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-04-30 12:16:47 +0200

    fixes for display

--- public/script.js
+++ public/script.js
@@ -1,16 +1,56 @@
-const button = document.querySelector('#messageButton')
+// const button = document.querySelector('#messageButton')
+//
+// button.addEventListener('click', event => {
+// 	console.log('Button clicked');
+//
+// 	fetch('http://localhost:1357/hamsters/random')
+// 	.then(response => response.json())
+// 	.then(data => {
+// 	    console.log(data)
+// 	})
+//
+// 	const messageElement = document.querySelector('.message')
+// 	messageElement.innerHTML += `<br> Random hamster in console!`
+//
+//
+// })
+
+
+const button = document.querySelector('#randomButton')
 
 button.addEventListener('click', event => {
-	console.log('Button clicked');
+ 	const url = 'https://fue20-hamsterwars.herokuapp.com//hamsters/random';
 
-	fetch('http://localhost:1357/hamsters/random')
-	.then(response => response.json())
-	.then(data => {
-	    console.log(data)
-	})
+	getData(url, showHamster);
 
-	const messageElement = document.querySelector('.message')
-	messageElement.innerHTML += `<br> Random hamster in console!`
+	function getData(URL, callback) {
+		fetch(URL)
+		.then(response => response.json())
+		.then(data => { callback(data); })
+		.catch(error => console.log(error));
+	}
 
+function showHamster(hamsters) {
+	const randomHamster = document.getElementById('hamster');
+
+		removeAllChildNodes(randomHamster);
+
+		const divTag = document.createElement('div');
+		const h3Tag = document.createElement('h3');
+		const imgTag = document.createElement('img');
+
+		h3Tag.innerText = hamsters.name;
+		imgTag.src = `../img/${hamsters.imgName}`;
+
+		randomHamster.appendChild(divTag);
+		divTag.appendChild(h3Tag);
+		divTag.appendChild(imgTag);
+	}
 
 })
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
